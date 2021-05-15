a2170b01a18d5b1a330c126f4d97669f396da46e
Oliver Strik <o.h.p.strik@researchable.nl>
2021-05-16 00:03:35 +0200

    chore: clean up imports

--- src/pages/james-allen.tsx
+++ src/pages/james-allen.tsx
@@ -1,9 +1,3 @@
-import Layout from "../components/Layout";
-import BasicMeta from "../components/meta/BasicMeta";
-import OpenGraphMeta from "../components/meta/OpenGraphMeta";
-import TwitterCardMeta from "../components/meta/TwitterCardMeta";
-import { SocialList } from "../components/SocialList";
-
 const JamesAllenPage = () => {
   return (<div>james_allen</div>);
 }
